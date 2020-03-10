7b211da2b614963da0e471700929405b1b84c22b
Ranie Jade Ramiso <raniejaderamiso@gmail.com>
2020-03-10 19:21:44 +1100

    Update benchmarks

--- src/commonMain/kotlin/benchmark/Allocation.kt
+++ src/commonMain/kotlin/benchmark/Allocation.kt
@@ -4,17 +4,37 @@ import kotlinx.benchmark.*
 
 @State(Scope.Benchmark)
 @OutputTimeUnit(BenchmarkTimeUnit.MILLISECONDS)
+@Measurement(time = 1, timeUnit = BenchmarkTimeUnit.SECONDS)
 @BenchmarkMode(Mode.Throughput)
 class Allocation {
-  @Param("10", "100", "1000")
-  var count = 0
+  @Param("30", "60", "120")
+  var fps = 0
+
+  lateinit var data: CharArray
+
+  @Setup
+  fun setup() {
+    // 1 kb of data
+    data = CharArray(512) { '0' }
+  }
 
   @Benchmark
-  fun allocate(): List<String> {
+  fun allocateAndKeep(): List<String> {
     val list = mutableListOf<String>()
-    for (i in 0 until count) {
-      list.add(i.toString())
+    for (i in 0 until fps) {
+      list.add(allocateData())
     }
     return list
+  }
+
+  @Benchmark
+  fun allocateAndThrowAway(bh: Blackhole) {
+    for (i in 0 until fps) {
+      bh.consume(allocateData())
+    }
+  }
+
+  private fun allocateData(): String {
+    return String(data)
   }
 }
--- src/commonMain/kotlin/benchmark/SimpleOps.kt
+++ src/commonMain/kotlin/benchmark/SimpleOps.kt
@@ -4,17 +4,52 @@ import kotlinx.benchmark.*
 
 @State(Scope.Benchmark)
 @OutputTimeUnit(BenchmarkTimeUnit.MILLISECONDS)
+@Measurement(time = 1, timeUnit = BenchmarkTimeUnit.SECONDS)
 @BenchmarkMode(Mode.Throughput)
 class SimpleOps {
-  private var a = 10
-  private var b = 20
+  @Param("30", "60", "120")
+  var fps = 0
+
+  var a = 0
+  var b = 0
+
+  @Setup
+  fun setup() {
+    a = 10
+    b = 20
+  }
 
   @Benchmark
-  fun sum(): Int {
-    var sum = 0
-    for (i in 0 until b) {
-      sum += a
+  fun add(bh: Blackhole) {
+    repeat(fps) {
+      bh.consume(a + b)
     }
-    return sum
+  }
+
+  @Benchmark
+  fun sub(bh: Blackhole) {
+    repeat(fps) {
+      bh.consume(a - b)
+    }
+  }
+
+  @Benchmark
+  fun mul(bh: Blackhole) {
+    repeat(fps) {
+      bh.consume(a * b)
+    }
+  }
+
+  @Benchmark
+  fun div(bh: Blackhole) {
+    repeat(fps) {
+      bh.consume(a / b)
+    }
+  }
+
+  private inline fun repeat(count: Int, cb: () -> Unit) {
+    for (i in 0 until count) {
+      cb()
+    }
   }
 }
